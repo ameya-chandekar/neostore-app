8db38e6a39229198aa50b4e69df9e6e052f574ab
ameya <ameya.chandekar@neosoftech.com>
2020-03-02 15:57:03 +0530

    created navbar component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function footer() {
+    return (
+        <div className="footer">
+            <div className="footer-wrapper">
+                <div className='row'>
+                    <div>
+                        <div className="col-lg-4"> 
+                        <h5>About Company</h5>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="col-lg-4"> 
+                        <h5>Information</h5></div>
+                    </div>
+
+                    <div>
+                        <div className="col-lg-4"><h5>NewsLetter</h5> </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default footer
---
+++ src/components/homepage/homePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Navbar from '../navbar/navbar'
+export class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        )
+    }
+}
+
+export default HomePage
